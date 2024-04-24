3ddf38b38b09b171b6ebec12b4ba5ee0f9e295dd
AeternaGate <Hipster23002003@gmail.com>
2024-04-24 23:22:38 +0300

    Добавьте файлы проекта.

---
+++ AeternoApp/LoginWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AeternoApp
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Login_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string password = passwordBoxPassword.Password.Trim();
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Login is too short";
+                textBoxLogin.BorderBrush = Brushes.Red;
+            }
+            else if (password.Length < 5)
+            {
+                passwordBoxPassword.ToolTip = "Password is too short";
+                passwordBoxPassword.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = null;
+                passwordBoxPassword.ToolTip = null;
+                textBoxLogin.BorderBrush = Brushes.Black;
+                passwordBoxPassword.BorderBrush = Brushes.Black;
+
+                User loginUser = null;
+                using (AppContext context = new AppContext()) 
+                {
+                    loginUser = context.Users.Where(u => u.Login == login && u.Password == password).FirstOrDefault();
+                }
+
+                if (loginUser != null)
+                {
+                    MessageBox.Show("Good!");
+
+                    UserPageWindow userPageWindow = new UserPageWindow();
+                    userPageWindow.Show();
+                    Hide();
+                }
+                else
+                    MessageBox.Show("User not found");
+            }
+        }
+
+        private void Button_Registration_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Hide();
+        }
+    }
+}
---
+++ AeternoApp/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AeternoApp
+{
+    public partial class MainWindow : Window
+    {
+
+        AppContext db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            db = new AppContext();
+        }
+
+        private void Button_Register_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string email = textBoxEmail.Text.Trim().ToLower();
+            string password = passwordBoxPassword.Password.Trim();
+            string repeatPassword = passwordBoxRepeatPassword.Password.Trim();
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Login is too short";
+                textBoxLogin.BorderBrush = Brushes.Red;
+            }
+            else if (password.Length < 5)
+            {
+                passwordBoxPassword.ToolTip = "Password is too short";
+                passwordBoxPassword.BorderBrush = Brushes.Red;
+            }
+            else if (repeatPassword != password)
+            {
+                passwordBoxRepeatPassword.ToolTip = "Passwords don't match";
+                passwordBoxRepeatPassword.BorderBrush = Brushes.Red;
+            }
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Wrong email";
+                textBoxEmail.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = null;
+                textBoxEmail.ToolTip = null;
+                passwordBoxPassword.ToolTip = null;
+                passwordBoxRepeatPassword.ToolTip = null;
+                textBoxLogin.BorderBrush = Brushes.Black;
+                textBoxEmail.BorderBrush = Brushes.Black;
+                passwordBoxPassword.BorderBrush = Brushes.Black;
+                passwordBoxRepeatPassword.BorderBrush = Brushes.Black;
+
+                MessageBox.Show("Good!");
+                User user = new User(login, password, email);
+
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.Show();
+                Hide();
+            }
+        }
+
+        private void Button_Window_Login_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+            Hide();
+        }
+    }
+}
